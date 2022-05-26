a889088369332b19011fc6fa370243f396b0cdc6
Antonio Pires <tonan@outlook.com.br>
2022-05-26 07:08:00 -0300

    Using mapState to map store state as computed props - part 1

--- src/store.js
+++ src/store.js
@@ -4,5 +4,13 @@ import Vuex from 'vuex';
 export default new Vuex.Store({
     state: {
         titulo: 'Emergências Médicas',
+        equipe: {
+            enfermeiro: 'Nome do enfermeiro',
+            socorrista: 'Nome do socorrista',
+            medico: 'Nome do médico',
+            carro: 'Placa do carro',
+            telefone: '+55 91 99999-1111',
+            kitDeReanimacao: 'Kit 0001',
+        },
     }
 });
