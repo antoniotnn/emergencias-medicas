1b98b323f09a013db7b5108bceddd07bfbb6e755
Antonio Pires <tonan@outlook.com.br>
2022-06-20 06:56:37 -0300

    Listando equipes montadas parte 1

---
+++ src/components/Equipe.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Listagem das Equipes
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Equipe',
+}
+</script>
---
+++ src/components/ListaEquipes.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <equipe />
+    </div>
+</template>
+
+<script>
+import Equipe from '@/components/Equipe'
+
+export default {
+    name: 'ListaEquipes',
+    components: {
+        Equipe
+    },
+}
+</script>
